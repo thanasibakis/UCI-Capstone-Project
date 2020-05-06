7e73eb7b11426ce2ec33a3dc3137ec7443afa0f3
Thanasi Bakis <thanasibakis@gmail.com>
2020-05-06 10:09:20 -0700

    Plots start at y-axis 0 now

--- EDA_sentiment_scores.R
+++ EDA_sentiment_scores.R
@@ -4,15 +4,23 @@ library(DBI)
 library(tidyverse)
 library(plotly)
 
-con <- dbConnect(
-	RPostgres::Postgres(),
-	host = SQL_HOST,
-	dbname = SQL_DB,
-	user = SQL_USER,
-	password = SQL_PASS
-)
+#con <- dbConnect(
+#	RPostgres::Postgres(),
+#	host = SQL_HOST,
+#	dbname = SQL_DB,
+#	user = SQL_USER,
+#	password = SQL_PASS
+#)
 
-data <- tbl(con, "dataset")
+#dataset <- tbl(con, "dataset") %>%
+#	collect()
+
+# dbDisconnect(con)
+
+#write_csv(dataset, "dataset.csv")
+
+data <- read_csv("dataset.csv") %>%
+	filter(lastpos != 0) # songs on the charts for the first time haven't changed rank
 
 
 ggplotly(
@@ -23,7 +31,8 @@ ggplotly(
 		ggplot(aes(x = newsmonth, y = value, grp = score, col = score)) +
 		geom_line() +
 		labs(title = "Sentiment Scores of News", x = "Month", y = "Sentiment Score") +
-		theme(text = element_text(size = 18))
+		theme(text = element_text(size = 18)) +
+		ylim(c(0, 0.3))
 )
 
 # news was most negative in the month of April 2013
@@ -40,11 +49,14 @@ ggplotly(
 		ggplot(aes(x = chartmonth, y = value, grp = score, col = score)) +
 		geom_line() +
 		labs(title = "Sentiment Scores of Song Charts", x = "Month", y = "Sentiment Score") +
-		theme(text = element_text(size = 18))
+		theme(text = element_text(size = 18)) +
+		ylim(c(0, 0.3))
 )
 
 # meanwhile, a massive decline in chart negativity begain in April 2013,
 # and a decently sharp increase in chart positivity coincides with it
 
-dbDisconnect(con)
+
+
+#dbDisconnect(con)
 

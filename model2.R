0f45e9f8cd45952359f63dd01b418e0f50d46b22
Thanasi Bakis <thanasibakis@gmail.com>
2020-05-13 19:58:36 -0700

    New model

--- model2.R
+++ model2.R
@@ -21,6 +21,7 @@ library(magrittr)
 library(plotROC)
 library(plotly)
 library(tidyverse)
+library(glmnet)
 
 
 # Retrieve the data saved from the database
@@ -28,36 +29,32 @@ dataset <- read_csv("dataset.csv") %>%
 	filter(lastpos != 0) # songs on the charts for the first time haven't changed rank
 
 
-# Reduce dimensionality of song-related features
-PCA_songs <- dataset %>%
+# Our interaction terms will be defined as principal components (linear combinations of the original features)
+PCA <- dataset %>%
 	select(songpositivity,   songnegativity, songneutrality, songsentiment,
 		   acousticness,     danceability,   duration_ms,    energy,
 		   instrumentalness, key,            liveness,       loudness,
 		   mode,             speechiness,    tempo,          time_signature,
 		   valence,          peakpos,        weeks,
-		   starts_with(c("songmin", "songmax"))) %>%
-	prcomp() %>%
-	magrittr::extract2("x") %>%
-	magrittr::extract(, 1:30) %>%
-	as.data.frame() %>%
-	setNames(paste0("song_", colnames(.)))
+		   starts_with(c("songmin", "songmax")),
 
-
-# Reduce dimensionality of news-related features
-PCA_news <- dataset %>%
-	select(newspositivity,   newsnegativity, newsneutrality, newssentiment,
+		   newspositivity,   newsnegativity, newsneutrality, newssentiment,
 		   starts_with(c("newsmin", "newsmax"))) %>%
-	prcomp() %>%
+	prcomp()
+
+features <- PCA %>%
 	magrittr::extract2("x") %>%
-	magrittr::extract(, 1:30) %>%
-	as.data.frame() %>%
-	setNames(paste0("news_", colnames(.)))
+	as.data.frame()
+
+
+# try joining news/music features then doing pca on all of them
+# so our intereactions are the linear combinations of the vars
+# and also still do penalized/regularization bc 900 features for 8000 rows
+# and also do pca plots
 
 
 # Generate the model formula with all the interaction terms
-formula <- expand_grid(PC_song = colnames(PCA_songs), PC_news = colnames(PCA_news)) %>%
-	mutate(interaction = paste(PC_song, PC_news, sep = ":")) %>%
-	pull(interaction) %>%
+formula <- colnames(features) %>%
 	paste(collapse = " + ") %>%
 	paste("response ~", . )
 
@@ -66,7 +63,7 @@ formula <- expand_grid(PC_song = colnames(PCA_songs), PC_news = colnames(PCA_new
 features <- dataset %>%
 	mutate(response = rank < lastpos) %>%
 	select(response) %>%
-	cbind(PCA_songs, PCA_news)
+	cbind(features)
 
 
 # Train-test split
@@ -77,18 +74,49 @@ testing_data <- anti_join(features, training_data)
 
 
 # Fit the model
-model <- glm(formula, data = training_data, family = "binomial")
-summary(model)
+#model <- glm(formula, data = training_data, family = "binomial")
+model <- cv.glmnet(
+	as.matrix(select(training_data, -response)),
+	pull(training_data, response),
+	family = "binomial"
+)
+
+plot(model)
+coef(model)@i # index 1 (value 0) is the intercept
+
+# https://stackoverflow.com/questions/29311323
 
 
 # Make predictions and assess accuracy
 predictions <- testing_data %>%
-	mutate(soft_prediction = predict.glm(model, newdata = ., type = "response")) %>%
+	mutate(soft_prediction = predict(model, newx = as.matrix(select(., -response)), type = "response")) %>%
 	mutate(hard_prediction = soft_prediction >= 0.5) %>%
 	mutate(is_correct      = hard_prediction == response)
 
-paste("Accuracy:", round(mean(predictions$is_correct), 2))
+paste("Accuracy:", round(mean(predictions$is_correct), 4))
 
+
+# What is our test data's response distribution?
+# What is our model's prediction distribution?
+ggplotly(
+	predictions %>%
+		select(response, hard_prediction) %>%
+		pivot_longer(
+			c(response, hard_prediction),
+			names_to = "Type",
+			values_to = "Value"
+		) %>%
+		ggplot(aes(x = Value)) +
+		geom_bar(aes(fill = Value)) +
+		ylim(0, nrow(predictions)) +
+		facet_wrap(
+			~ Type,
+			labeller = labeller(Type = c(hard_prediction = "Model Prediction", response = "True Value"))
+		) +
+		labs(title = "Distribution of response values and model predictions"),
+	tooltip = c("y")
+) %>%
+	hide_legend()
 
 # How good are the hard predictions?
 ggplotly(
@@ -112,4 +140,5 @@ ggplotly(
 # ROC curve to figure out which soft-->hard cutoff to use
 predictions %>%
 	ggplot(aes(m = soft_prediction, d = response)) +
-	geom_roc(n.cuts = 20, labels = T, labelround = 2)
+	geom_roc(n.cuts = 20, labels = T, labelround = 2) +
+	geom_abline(a = 1, b = 0)

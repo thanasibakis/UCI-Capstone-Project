ade491e2c588d762295111aafd164b3eac4dcead
Thanasi Bakis <thanasibakis@gmail.com>
2020-05-15 11:19:29 -0700

    Generating dataset.csv is now its own R file

--- model.R
+++ model.R
@@ -1,22 +1,3 @@
-# Load the data from the database to a local CSV
-#library(DBI)
-#source("api_keys.py")
-
-#con <- dbConnect(
-#	RPostgres::Postgres(),
-#	host = SQL_HOST,
-#	dbname = SQL_DB,
-#	user = SQL_USER,
-#	password = SQL_PASS
-#)
-
-#dataset <- tbl(con, "dataset") %>%
-#	collect()
-
-#dbDisconnect(con)
-#write_csv(dataset, "dataset.csv")
-
-
 library(magrittr)
 library(plotROC)
 library(plotly)
